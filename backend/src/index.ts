0e661302b0b482ca3be8367e679d34fd92be742e
Alan Bausano <alan.bausano@gmail.com>
2026-03-20 16:42:36 -0300

    modified allowed origins

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -15,7 +15,7 @@ const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:3001',
   process.env.FRONTEND_URL
-].filter(Boolean) as string[];
+]
 
 app.use(cors({
   origin: (origin, callback) => {
